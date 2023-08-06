60b0f9b37e86070a5db934578afa5e50df032fdd
Gumadan <l.fral1993@gmail.com>
2023-08-06 20:24:46 +0400

    Инпут реагирует на пустоту

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -5,21 +5,31 @@ import "./TaskInput.css";
 
 const TaskInput = (props) => {
   const [inputText, setinputText] = useState("");
+  const [isInputValid, setIsInputvalid] = useState(true);
 
   const taskInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsInputvalid(true);
+    }
     setinputText(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (inputText.trim().length === 0) {
+      setIsInputvalid(false);
+      return;
+    }
     props.onAddTask(inputText);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isInputValid ? 'invalid' : ''}`}>
         <label>Задачи</label>
-        <input type="text" onChange={taskInputChangeHandler} />
+        <input type="text"
+          onChange={taskInputChangeHandler}
+        />
       </div>
       <Button type="submit">Добавить Задачу</Button>
     </form>
